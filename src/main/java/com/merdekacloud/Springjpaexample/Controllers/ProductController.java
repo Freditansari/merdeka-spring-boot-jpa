ca771a380e008413c2aba1efa7b60bc60e4fe075
Freditansari <Freditansari@users.noreply.github.com>
2020-01-15 16:08:53 +0700

    completed everything

--- src/main/java/com/merdekacloud/Springjpaexample/Controllers/ProductController.java
+++ src/main/java/com/merdekacloud/Springjpaexample/Controllers/ProductController.java
@@ -5,16 +5,26 @@ import com.merdekacloud.Springjpaexample.Repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
 @RequestMapping("/product")
+@CrossOrigin
 public class ProductController {
     @Autowired
     ProductRepo productRepo;
 
     @GetMapping("/")
     public List<Product> getAll(){ return productRepo.findAll();}
+
+    @GetMapping("/price")
+    public List<Product> findGreaterThanPrice(@RequestParam("price") BigDecimal price){
+
+
+        return productRepo.findByProductPriceGreaterThan(price);
+    }
+
 
     @PostMapping("/")
     public Product save(@RequestBody Product product){ return productRepo.save(product);}
--- src/main/java/com/merdekacloud/Springjpaexample/Repository/ProductRepo.java
+++ src/main/java/com/merdekacloud/Springjpaexample/Repository/ProductRepo.java
@@ -5,6 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+//https://docs.spring.io/spring-data/jpa/docs/1.3.4.RELEASE/reference/html/jpa.repositories.html
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Long> {
+    List<Product> findByProductPriceGreaterThan(BigDecimal price);
 }
